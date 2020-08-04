0df1cfa235327efe0969863c0e6cb2aba3e5bea4
Galen Song <young_galenSong@163.com>
2020-08-04 23:42:31 +0800

    提交toyReact生命周期作业

--- ToyReact.js
+++ ToyReact.js
@@ -4,15 +4,32 @@ class ElementWrapper {
     }
 
     setAttribute(name, value) {
+        if(name.match(/^on([\s\S]+)$/)) {
+            let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase())
+            this.root.addEventListener(eventName, value)
+            // console.log(RegExp.$1)
+        }
+        if(name === "className") {
+            this.root.setAttribute('class', value)
+        }
         this.root.setAttribute(name, value)
     }
 
     appendChild(vchild) {
-        vchild.mountTo(this.root)
+        let range = document.createRange()
+        if(this.root.children.length) {
+            range.setStartAfter(this.root.lastChild)
+            range.setEndAfter(this.root.lastChild)
+        } else {
+            range.setStart(this.root, 0)
+            range.setEnd(this.root, 0)
+        }
+        vchild.mountTo(range)
     }
 
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    mountTo(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
@@ -21,24 +38,81 @@ class TextWrapper {
         this.root = document.createTextNode(content)
     }
 
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    mountTo(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
+        // parent.appendChild(this.root)
     }
 }
 
 export class Component {
     constructor() {
         this.children = []
+        this.props = Object.create(null)
+        this.lifeCycle = void 0
     }
     setAttribute(name, value) {
+        this.props[name] = value
         this[name] = value
     }
-    mountTo(parent) {
+    mountTo(range) {
+        if(this.componentWillMount && this.lifeCycle === void 0) {
+            this.componentWillMount()
+            this.lifeCycle = "componentWillMount"
+        }
+        this.range = range
+        this.update()
+        if(this.componentDidMount && this.lifeCycle === "componentWillMount") {
+            this.componentDidMount()
+            this.lifeCycle = "componentDidMount"
+        }
+    }
+    update() {
+        let placeholder = document.createElement('placeholder')
+        let range = document.createRange()
+        range.setStart(this.range.endContainer, this.range.endOffset)
+        range.setEnd(this.range.endContainer, this.range.endOffset)
+        range.insertNode(placeholder)
+        if(this.componentWillUnmount && (this.lifeCycle === "componentDidMount" || this.lifeCycle === "componentDidUpdate")) {
+            this.componentWillUnmount()
+            this.lifeCycle = 'componentWillUnmount'
+        }
+        this.range.deleteContents()
         let vdom = this.render()
-        vdom.mountTo(parent)
+        vdom.mountTo(this.range)
+
+        // placeholder.parentNode.removeChild(placeholder)
     }
     appendChild(vchild) {
         this.children.push(vchild)
+    }
+    setState(state) {
+        let merge = (oldState, newState) => {
+            for(let p in newState) {
+                if(typeof newState[p] === "object") {
+                    if(typeof oldState[p] !== "object") {
+                        oldState[p] = {}
+                    }
+                    merge(oldState[p], newState[p])
+                } else {
+                    oldState[p] = newState[p]
+                }
+            }
+        }
+        if(!this.state && state) {
+            this.state = {}
+        }
+        merge(this.state, state)
+        if(this.componentWillUpdate && (this.lifeCycle === "componentDidMount" || this.lifeCycle === "componentDidUpdate")) {
+            this.componentWillUpdate()
+            this.lifeCycle = "componentWillUpdate"
+        }
+        this.update()
+        if(this.componentDidUpdate && this.lifeCycle === "componentWillUpdate") {
+            this.componentDidUpdate()
+            this.lifeCycle = "componentDidUpdate"
+        }
+        // console.log(this.state)
     }
 }
 
@@ -73,10 +147,18 @@ export let ToyReact = {
             }
         }
         insertChildren(children)
-        debugger
         return element
     },
     render(vdom, element) {
-        vdom.mountTo(element)
+        let range = document.createRange()
+        if(element.children.length) {
+            range.setStartAfter(element.lastChild)
+            range.setEndAfter(element.lastChild)
+        } else {
+            range.setStart(element, 0)
+            range.setEnd(element, 0)
+        }
+        
+        vdom.mountTo(range)
     }
 }
--- main.js
+++ main.js
@@ -1,21 +1,76 @@
 import {ToyReact, Component} from './ToyReact'
 
-class MyComponent extends Component {
-    render() {
-        return <div>
-            <span>Hello</span>
-            <span>World</span>
-            <div>
-                {this.children}
-            </div>
-        </div>
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
     }
 
+    componentWillMount() {
+        console.log('Component WILL MOUNT!')
+
+    }
+
+    componentDidMount() {
+        this.setState({
+            value: 3
+        })
+    }
+
+    componentWillUpdate() {
+        console.log('Component WILL UPDATE!');
+    }
+
+    componentDidUpdate () {
+        console.log('Component DID UPDATE!')
+    }
+
+    componentWillUnmount() {
+        console.log('Component WILL UNMOUNT!')
+    }
+
+    render() {
+        return (
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+            {this.state.value}
+        </button>
+        );
+    }
 }
 
-let a = <MyComponent name="a">
-        <div>{123}</div>
-    </MyComponent>
+class Board extends Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+        return (
+          <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </div>
+          </div>
+        );
+      }
+}
+
+let a = <Board></Board>
 
 ToyReact.render(
     a,
